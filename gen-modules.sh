8b291a2ea76cb0ae76091d49c8aeab20aab2d7d5
Samuel Schlesinger <samuel@simspace.com>
2020-10-18 04:04:15 -0400

    Regenned modules

--- gen-modules.sh
+++ gen-modules.sh
@@ -1,1 +1,1 @@
-graphmod --remove-module=Main haskell-server-template.cabal | dot -Tpng -Gdpi=300 -o modules.png
+graphmod --remove-module=Server.Prelude --remove-module=Main haskell-server-template.cabal | dot -Tpng -Gdpi=300 -o modules.png
